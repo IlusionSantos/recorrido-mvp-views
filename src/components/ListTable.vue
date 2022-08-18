03d6ba3d9ab1ec623ae682da37f668d1a1cc50f1
IlusionSantos <ilusion.santosm@gmail.com>
2022-08-18 17:03:52 -0600

    List component

---
+++ src/components/ListTable.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "ListTable",
+  props: {
+    hours: Array,
+    day: String,
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <table class="table">
+    <tbody>
+      <tr v-for="(hour, index) in hours" :key="'hour_' + index">
+        <th>
+          <p class="has-text-black is-size-6 m-auto">{{ hour.hour_range }}</p>
+        </th>
+        <th>
+          <p v-if="hour.user_name" class="has-text-black is-size-6 m-auto">
+            {{ hour.user_name }}
+          </p>
+          <img v-else src="../assets//alert.png" width="28" height="28" />
+        </th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+tr {
+  th {
+    vertical-align: middle;
+    text-align: center;
+  }
+}
+</style>
