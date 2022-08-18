8e41870c671b65d3f751dde7e311cb76bcb112e6
IlusionSantos <ilusion.santosm@gmail.com>
2022-08-18 17:04:00 -0600

    Delete bulma reference

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -6,14 +6,6 @@ export default {
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <img
-          src="https://bulma.io/images/bulma-logo.png"
-          width="112"
-          height="28"
-        />
-      </a>
-
       <a
         role="button"
         class="navbar-burger"
